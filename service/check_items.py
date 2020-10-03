f1b96b8b5017782ea0bdb96c86ffda66c8475a1c
alestan.silva <alestan.silva@zanthus.com.br>
2020-10-03 20:35:49 -0300

    edit

--- service/check_items.py
+++ service/check_items.py
@@ -7,7 +7,6 @@ import psutil
 system_server = sys.platform
 service = os.popen("cat /etc/services").read()
 dns = os.popen("cat /etc/resolv.conf").read()
-system_info = os.popen("lsb_release -a").read()
 qtd_users = os.popen("getent passwd | wc -l").read()
 ssh = os.popen("systemctl status ssh.service").read()
 
@@ -16,13 +15,15 @@ json_server_items = {
     "path_archive_python": (os.getcwd()),
     "qtd_users": qtd_users.replace('\n', ''),
     "dns": dns.replace('\n', ''),
-    "system_info" : system_info.replace('\n', ''),
     "ssh_info" : ssh.replace('\n', ''),
-    "cpu" : psutil.cpu_times(),
+    "cpu" : psutil.cpu_percent(1),
     "virtual_memory": psutil.virtual_memory(),
     "users": psutil.users()
 }
 
 app_json = json.dumps(json_server_items)
 
-print(app_json)
+with open('archives/result.json', 'w') as fp:
+    json.dump(json_server_items, fp)
+
+print("result.json created in /archives")
